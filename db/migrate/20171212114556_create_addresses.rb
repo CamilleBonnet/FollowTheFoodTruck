0b67e1f6d638ed8522f68a6ebeeddf38d6f76ae3
lucas hamani <hamanilucas@gmail.com>
2017-12-12 12:49:59 +0100

    add address model and controller, no routes

---
+++ db/migrate/20171212114556_create_addresses.rb
@@ -0,0 +1,17 @@
+class CreateAddresses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :addresses do |t|
+      t.references :truck, foreign_key: true
+      t.text :street_address
+      t.text :street_address_2
+      t.string :city
+      t.integer :zipcode
+      t.string :country
+      t.text :more_info
+      t.decimal :latitude
+      t.decimal :longitude
+
+      t.timestamps
+    end
+  end
+end

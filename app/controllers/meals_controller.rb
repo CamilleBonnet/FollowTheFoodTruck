c0e5aafa6c365dd0caeaf896a615b5b31fbac3e0
lucas hamani <hamanilucas@gmail.com>
2017-12-12 12:35:20 +0100

    add meals controller with basic crud minus index and show

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,40 @@
+class MealsController < ApplicationController
+  before_action :set_meal, only: [:edit, :update, :destroy]
+  before_action :set_truck, only: [:create, :edit, :update, :destroy]
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = meal.new(meal_params)
+    @meal.truck = @truck
+    @meal.save
+  end
+
+  def edit
+  end
+
+  def update
+    @meal.update(meal_params)
+  end
+
+  def destroy
+    @meal.destroy
+  end
+
+  private
+
+  def set_meal
+    @meal = Meal.find(params[:id])
+  end
+
+  def set_truck
+    @truck = Truck.find(params[:id])
+  end
+
+  def meal_params
+    params.require(:meal).permit(:description, :is_vegan, :is_fat_free,:is_alergen, :is_gluten_free, :spicy_scale, :price, :truck_id, :photo, :photo_cache)
+  end
+
+end

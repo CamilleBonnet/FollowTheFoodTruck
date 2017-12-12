b94c326e909f51be922747b990548746800a26f6
Camille Bonnet <bonnet.cam@gmail.com>
2017-12-12 13:12:57 +0100

    registration with user model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212111113) do
+ActiveRecord::Schema.define(version: 20171212114229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,4 +32,16 @@ ActiveRecord::Schema.define(version: 20171212111113) do
     t.index ["reset_password_token"], name: "index_registrations_on_reset_password_token", unique: true
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.string "role"
+    t.bigint "registration_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["registration_id"], name: "index_users_on_registration_id"
+  end
+
+  add_foreign_key "users", "registrations"
 end

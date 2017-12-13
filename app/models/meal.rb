fa4d94aea5dec711039b346cdf2021d8af7801e0
lucas hamani <hamanilucas@gmail.com>
2017-12-13 18:05:42 +0100

    add country select gem and collection for country form

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,5 +2,7 @@ class Meal < ApplicationRecord
   belongs_to :truck
   has_many :choices
 
+  validates :price, presence: true
+  validates_numericality_of :price, greater_than_or_equal_to: 100
   has_attachment :photo
 end

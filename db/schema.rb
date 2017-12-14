6762912ccc29e11d6067d3386300aa7a553fbfe2
Camille Bonnet <bonnet.cam@gmail.com>
2017-12-14 20:06:28 +0100

    init commit for branch

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20171214163110) do
   create_table "baskets", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "truck_order_list_id"
-    t.integer  "total_price"
+    t.float    "total_price"
     t.date     "date"
     t.datetime "time"
     t.string   "status"

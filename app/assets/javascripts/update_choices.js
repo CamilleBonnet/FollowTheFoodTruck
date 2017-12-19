6ce1d10fa6ad5e849f5ff5efa6c0337f2acb3864
Camille Bonnet <bonnet.cam@gmail.com>
2017-12-19 11:12:49 +0100

    javascript path done. API to do

--- app/assets/javascripts/update_choices.js
+++ app/assets/javascripts/update_choices.js
@@ -4,22 +4,38 @@ var allPlusQuantity = document.querySelectorAll(".ftft-plus.ftft-choice.fa-stack
 var allMinusQuantity = document.querySelectorAll(".ftft-minus.ftft-choice.fa-stack")
 
 
+function onClickPlus () {
+
+  var meal_id = this.getAttribute("data-meal-id");
+  var number = Number.parseInt(this.nextElementSibling.innerHTML, 10) + 1;
+  sendPostRequest(meal_id, number);
+
+}
+
+function onClickMinus () {
+
+  var meal_id = this.getAttribute("data-meal-id");
+  var number = Number.parseInt(this.previousElementSibling.innerHTML, 10) - 1;
+  if (number < 0) {
+    number = 0;
+  }
+  sendPostRequest(meal_id, number);
+}
+
 function sendPostRequest (meal_id, value) {
-    // $post("/api/v1/update_meal", { meal_id: meal, quantity: value }, function(data) {
-    //   quantity.innerHTML = data["quantity"]
-    // })
-    console.log(meal_id);
-    console.log(value);
-  this.classList.toggle('fa-stack');
+  // $post("/api/v1/update_meal", { meal_id: meal, quantity: value }, function(data) {
+  //   quantity.innerHTML = data["quantity"]
+  // })
+  console.log(meal_id);
+  console.log(value);
 }
 
 function eventManager(){
   allPlusQuantity.forEach((btn) => {
-    console.log(btn)
-    btn.addEventListener("click", sendPostRequest(btn.data("data-meal-id"), btn.innerHTML + 1));
+    btn.addEventListener("click", onClickPlus);
   })
   allMinusQuantity.forEach((btn) => {
-    btn.addEventListener("click", sendPostRequest());
+    btn.addEventListener("click", onClickMinus);
   })
 }
 

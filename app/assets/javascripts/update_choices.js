23ef88b6bb141d068696ead9017d5599e0709f3b
Camille Bonnet <bonnet.cam@gmail.com>
2017-12-19 10:44:43 +0100

    api half done

--- app/assets/javascripts/update_choices.js
+++ app/assets/javascripts/update_choices.js
@@ -1,25 +1,29 @@
-function init(){
+// var plusQuantity = document.getElementById("arrow-plus");
+// var minusQuantity = document.getElementById("arrow-minus");
+var allPlusQuantity = document.querySelectorAll(".ftft-plus.ftft-choice.fa-stack")
+var allMinusQuantity = document.querySelectorAll(".ftft-minus.ftft-choice.fa-stack")
 
-  var plusQuantity = document.getElementById("arrow-plus");
 
-  plusQuantity.addEventListener("click", function() {
-  //   $post("/api/v1/update_meal", { meal_id: quantity.data("meal-id"), quantity: (quantity.innerHTML +1) }, function(data) {
-  //     quantity.innerHTML = data["quantity"]
-  //   })
-    plusQuantity.classList.remove('fa-stack');
-  });
+function sendPostRequest (meal_id, value) {
+    // $post("/api/v1/update_meal", { meal_id: meal, quantity: value }, function(data) {
+    //   quantity.innerHTML = data["quantity"]
+    // })
+    console.log(meal_id);
+    console.log(value);
+  this.classList.toggle('fa-stack');
+}
 
-  var minusQuantity = document.getElementById("arrow-minus");
-
-  minusQuantity.addEventListener("click", function() {
-  //   $post("/api/v1/update_meal", { meal_id: quantity.data("meal-id"), quantity: (quantity.innerHTML +1) }, function(data) {
-  //     quantity.innerHTML = data["quantity"]
-  //   })
-  minusQuantity.classList.remove('fa-stack');
-  });
+function eventManager(){
+  allPlusQuantity.forEach((btn) => {
+    console.log(btn)
+    btn.addEventListener("click", sendPostRequest(btn.data("data-meal-id"), btn.innerHTML + 1));
+  })
+  allMinusQuantity.forEach((btn) => {
+    btn.addEventListener("click", sendPostRequest());
+  })
 }
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  init();
+  eventManager();
 });

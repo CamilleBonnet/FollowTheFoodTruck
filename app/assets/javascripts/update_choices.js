6718d2d2aa2128723f0ba91e2df891ad0a0a0a83
Camille Bonnet <bonnet.cam@gmail.com>
2017-12-20 16:35:58 +0100

    disable JS files

--- app/assets/javascripts/update_choices.js
+++ app/assets/javascripts/update_choices.js
@@ -1,73 +1,73 @@
 
-function onClickPlus () {
-  var userAndMealInfo = document.getElementById("user-and-meal-info");
-  var email = userAndMealInfo.getAttribute("data-user-mail");
-  var token = userAndMealInfo.getAttribute("data-user-token");
+// function onClickPlus () {
+//   var userAndMealInfo = document.getElementById("user-and-meal-info");
+//   var email = userAndMealInfo.getAttribute("data-user-mail");
+//   var token = userAndMealInfo.getAttribute("data-user-token");
 
 
-  var meal_id = this.getAttribute("data-meal-id");
-  var elementToChange = this.nextElementSibling
-  var change = 1
-  var number = Number.parseInt(elementToChange.innerHTML, 10) + change;
-  sendPostRequestChoice(meal_id, number, email, token, elementToChange, change);
-}
+//   var meal_id = this.getAttribute("data-meal-id");
+//   var elementToChange = this.nextElementSibling
+//   var change = 1
+//   var number = Number.parseInt(elementToChange.innerHTML, 10) + change;
+//   sendPostRequestChoice(meal_id, number, email, token, elementToChange, change);
+// }
 
-function onClickMinus () {
-  var userAndMealInfo = document.getElementById("user-and-meal-info");
-  var email = userAndMealInfo.getAttribute("data-user-mail");
-  var token = userAndMealInfo.getAttribute("data-user-token");
+// function onClickMinus () {
+//   var userAndMealInfo = document.getElementById("user-and-meal-info");
+//   var email = userAndMealInfo.getAttribute("data-user-mail");
+//   var token = userAndMealInfo.getAttribute("data-user-token");
 
 
-  var meal_id = this.getAttribute("data-meal-id");
-  var elementToChange = this.previousElementSibling
-  var change = -1
-  var number = Number.parseInt(elementToChange.innerHTML, 10) + change;
-  if (number >= 0) {
-    sendPostRequestChoice(meal_id, number,email, token, elementToChange, change);
-  }
-}
+//   var meal_id = this.getAttribute("data-meal-id");
+//   var elementToChange = this.previousElementSibling
+//   var change = -1
+//   var number = Number.parseInt(elementToChange.innerHTML, 10) + change;
+//   if (number >= 0) {
+//     sendPostRequestChoice(meal_id, number,email, token, elementToChange, change);
+//   }
+// }
 
-function sendPostRequestChoice (meal_id, value, email, token, elementToChange, change) {
-  var myBody = {
-    meal_id: meal_id,
-    quantity: value
-  };
+// function sendPostRequestChoice (meal_id, value, email, token, elementToChange, change) {
+//   var myBody = {
+//     meal_id: meal_id,
+//     quantity: value
+//   };
 
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("X-Registration-Email", email);
-  myHeaders.append("X-Registration-Token", token);
+//   var myHeaders = new Headers();
+//   myHeaders.append("Content-Type", "application/json");
+//   myHeaders.append("X-Registration-Email", email);
+//   myHeaders.append("X-Registration-Token", token);
 
-  var myRequest = {method: 'POST',
-                 headers: myHeaders,
-                 body: JSON.stringify(myBody)};
+//   var myRequest = {method: 'POST',
+//                  headers: myHeaders,
+//                  body: JSON.stringify(myBody)};
 
-  fetch("/api/v1/update_choice", myRequest).then(
-    function(answer) {
-      elementToChange.innerHTML = value
-      var btnValue = document.getElementById("order-btn")
-      var newBtn = Number.parseInt((/\d+/.exec(btnValue.innerHTML))[0], 10) + change
-      btnValue.innerHTML = "Order (".concat(newBtn,")")
-      if (newBtn == 0){
-        btnValue.classList.add('disabled');
-      } else{
-        btnValue.classList.remove('disabled');
-      }
-    });
-}
+//   fetch("/api/v1/update_choice", myRequest).then(
+//     function(answer) {
+//       elementToChange.innerHTML = value
+//       var btnValue = document.getElementById("order-btn")
+//       var newBtn = Number.parseInt((/\d+/.exec(btnValue.innerHTML))[0], 10) + change
+//       btnValue.innerHTML = "Order (".concat(newBtn,")")
+//       if (newBtn == 0){
+//         btnValue.classList.add('disabled');
+//       } else{
+//         btnValue.classList.remove('disabled');
+//       }
+//     });
+// }
 
-function eventManagerChoice(){
-  var allPlusQuantity = document.querySelectorAll(".ftft-plus.ftft-choice.fa-stack");
-  var allMinusQuantity = document.querySelectorAll(".ftft-minus.ftft-choice.fa-stack");
+// function eventManagerChoice(){
+//   var allPlusQuantity = document.querySelectorAll(".ftft-plus.ftft-choice.fa-stack");
+//   var allMinusQuantity = document.querySelectorAll(".ftft-minus.ftft-choice.fa-stack");
 
-  allPlusQuantity.forEach(function(btn) {
-    btn.addEventListener("click", onClickPlus);
-  });
-  allMinusQuantity.forEach(function(btn) {
-    btn.addEventListener("click", onClickMinus);
-  });
-}
+//   allPlusQuantity.forEach(function(btn) {
+//     btn.addEventListener("click", onClickPlus);
+//   });
+//   allMinusQuantity.forEach(function(btn) {
+//     btn.addEventListener("click", onClickMinus);
+//   });
+// }
 
-document.addEventListener("DOMContentLoaded", function() {
-  eventManagerChoice();
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//   eventManagerChoice();
+// });

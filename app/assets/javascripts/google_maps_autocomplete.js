29703d2aeda1d99318cfb6f84a96697177260200
Camille Bonnet <bonnet.cam@gmail.com>
2017-12-20 16:42:56 +0100

    test with all the JS file

--- app/assets/javascripts/google_maps_autocomplete.js
+++ app/assets/javascripts/google_maps_autocomplete.js
@@ -1,89 +1,89 @@
-// function onPlaceChanged() {
-//   var place = this.getPlace();
-//   var components = getAddressComponents(place);
+function onPlaceChanged() {
+  var place = this.getPlace();
+  var components = getAddressComponents(place);
 
-//   var searchAddress = document.getElementById('food_truck_address');
-//   // searchAddress.blur();
+  var searchAddress = document.getElementById('food_truck_address');
+  // searchAddress.blur();
 
-//   var newFoodTruckAddress = document.getElementById('address_street_address');
-//   newFoodTruckAddress.value = components.address;
+  var newFoodTruckAddress = document.getElementById('address_street_address');
+  newFoodTruckAddress.value = components.address;
 
-//   var newFoodTruckAddressZipCode = document.getElementById('address_zipcode');
-//   newFoodTruckAddressZipCode.value = components.zip_code;
+  var newFoodTruckAddressZipCode = document.getElementById('address_zipcode');
+  newFoodTruckAddressZipCode.value = components.zip_code;
 
-//   var newFoodTruckAddressCity =  document.getElementById('address_city');
-//   newFoodTruckAddressCity.value = components.city;
+  var newFoodTruckAddressCity =  document.getElementById('address_city');
+  newFoodTruckAddressCity.value = components.city;
 
-//   if (components.country_code) {
-//     var selector = '#address_country option[value="' + components.country_code + '"]';
-//     document.querySelector(selector).selected = true;
-//   }
-// }
+  if (components.country_code) {
+    var selector = '#address_country option[value="' + components.country_code + '"]';
+    document.querySelector(selector).selected = true;
+  }
+}
 
-// function getAddressComponents(place) {
-//   // If you want lat/lng, you can look at:
-//   // - place.geometry.location.lat()
-//   // - place.geometry.location.lng()
+function getAddressComponents(place) {
+  // If you want lat/lng, you can look at:
+  // - place.geometry.location.lat()
+  // - place.geometry.location.lng()
 
-//   if (window.console && typeof console.log === "function") {
-//     // console.log(place);
-//   }
+  if (window.console && typeof console.log === "function") {
+    // console.log(place);
+  }
 
-//   var street_number = null;
-//   var route = null;
-//   var zip_code = null;
-//   var city = null;
-//   var country_code = null;
-//   for (var i in place.address_components) {
-//     var component = place.address_components[i];
-//     for (var j in component.types) {
-//       var type = component.types[j];
-//       if (type === 'street_number') {
-//         street_number = component.long_name;
-//       } else if (type === 'route') {
-//         route = component.long_name;
-//       } else if (type === 'postal_code') {
-//         zip_code = component.long_name;
-//       } else if (type === 'locality') {
-//         city = component.long_name;
-//       } else if (type === 'postal_town' && city === null) {
-//         city = component.long_name;
-//       } else if (type === 'country') {
-//         country_code = component.short_name;
-//       }
-//     }
-//   }
+  var street_number = null;
+  var route = null;
+  var zip_code = null;
+  var city = null;
+  var country_code = null;
+  for (var i in place.address_components) {
+    var component = place.address_components[i];
+    for (var j in component.types) {
+      var type = component.types[j];
+      if (type === 'street_number') {
+        street_number = component.long_name;
+      } else if (type === 'route') {
+        route = component.long_name;
+      } else if (type === 'postal_code') {
+        zip_code = component.long_name;
+      } else if (type === 'locality') {
+        city = component.long_name;
+      } else if (type === 'postal_town' && city === null) {
+        city = component.long_name;
+      } else if (type === 'country') {
+        country_code = component.short_name;
+      }
+    }
+  }
 
-//   return {
-//     address: street_number === null ? route : (street_number + ' ' + route),
-//     zip_code: zip_code,
-//     city: city,
-//     country_code: country_code
-//   };
-// }
+  return {
+    address: street_number === null ? route : (street_number + ' ' + route),
+    zip_code: zip_code,
+    city: city,
+    country_code: country_code
+  };
+}
 
-// document.addEventListener("DOMContentLoaded", function() {
-//   var foodTruckAddress = document.getElementById('food_truck_address');
+document.addEventListener("DOMContentLoaded", function() {
+  var foodTruckAddress = document.getElementById('food_truck_address');
 
-//   if (foodTruckAddress) {
-//     var autocomplete = new google.maps.places.Autocomplete(foodTruckAddress, { types: ['geocode'] });
-//     google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-//     google.maps.event.addDomListener(foodTruckAddress, 'keydown', function(e) {
-//       if (e.key === "Enter") {
-//         e.preventDefault(); // Do not submit the form on Enter.
-//       }
-//     });
-//   }
+  if (foodTruckAddress) {
+    var autocomplete = new google.maps.places.Autocomplete(foodTruckAddress, { types: ['geocode'] });
+    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+    google.maps.event.addDomListener(foodTruckAddress, 'keydown', function(e) {
+      if (e.key === "Enter") {
+        e.preventDefault(); // Do not submit the form on Enter.
+      }
+    });
+  }
 
-//   var foodTruckNewAddress = document.getElementById('address_street_address');
+  var foodTruckNewAddress = document.getElementById('address_street_address');
 
-//   if (foodTruckNewAddress) {
-//     var autocomplete = new google.maps.places.Autocomplete(foodTruckNewAddress, { types: ['geocode'] });
-//     google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-//     google.maps.event.addDomListener(foodTruckNewAddress, 'keydown', function(e) {
-//       if (e.key === "Enter") {
-//         e.preventDefault(); // Do not submit the form on Enter.
-//       }
-//     });
-//   }
-// });
+  if (foodTruckNewAddress) {
+    var autocomplete = new google.maps.places.Autocomplete(foodTruckNewAddress, { types: ['geocode'] });
+    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+    google.maps.event.addDomListener(foodTruckNewAddress, 'keydown', function(e) {
+      if (e.key === "Enter") {
+        e.preventDefault(); // Do not submit the form on Enter.
+      }
+    });
+  }
+});
--- app/assets/javascripts/update_status_order.js
+++ app/assets/javascripts/update_status_order.js
@@ -1,69 +1,69 @@
-// function sendPostRequestStatus (basketId, email, token, status, elementToChange) {
-//   var myBody = {
-//     basket_id: basketId,
-//     status: status
-//   };
+function sendPostRequestStatus (basketId, email, token, status, elementToChange) {
+  var myBody = {
+    basket_id: basketId,
+    status: status
+  };
 
-//   var myHeaders = new Headers();
-//   myHeaders.append("Content-Type", "application/json");
-//   myHeaders.append("X-Registration-Email", email);
-//   myHeaders.append("X-Registration-Token", token);
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("X-Registration-Email", email);
+  myHeaders.append("X-Registration-Token", token);
 
-//   var myRequest = { method: 'POST',
-//                     headers: myHeaders,
-//                     body: JSON.stringify(myBody)
-//                   };
+  var myRequest = { method: 'POST',
+                    headers: myHeaders,
+                    body: JSON.stringify(myBody)
+                  };
 
-//   fetch("/api/v1/update_status", myRequest).then(
-//     function(answer) {
-//       if (status == "Accepted by FoodTruck" ){
-//         elementToChange.innerHTML = status
-//         elementToChange.classList.add("accepted");
-//         elementToChange.classList.remove("declined");
-//       } else if (status == "Declined by FoodTruck") {
-//         elementToChange.innerHTML = status
-//         elementToChange.classList.remove("accepted");
-//         elementToChange.classList.remove("pending");
-//         elementToChange.classList.add("declined");
-//       }
-//     });
-// }
+  fetch("/api/v1/update_status", myRequest).then(
+    function(answer) {
+      if (status == "Accepted by FoodTruck" ){
+        elementToChange.innerHTML = status
+        elementToChange.classList.add("accepted");
+        elementToChange.classList.remove("declined");
+      } else if (status == "Declined by FoodTruck") {
+        elementToChange.innerHTML = status
+        elementToChange.classList.remove("accepted");
+        elementToChange.classList.remove("pending");
+        elementToChange.classList.add("declined");
+      }
+    });
+}
 
-// function onClickAccept () {
-//   var userAndBasketInfo = document.getElementById("user-and-basket-info");
-//   var email = userAndBasketInfo.getAttribute("data-user-mail");
-//   var token = userAndBasketInfo.getAttribute("data-user-token");
-//   var basketId = this.getAttribute("data-basket-id");
-//   var elementToChange = document.getElementById("basket-status-".concat(basketId))
-//   var status = "Accepted by FoodTruck";
+function onClickAccept () {
+  var userAndBasketInfo = document.getElementById("user-and-basket-info");
+  var email = userAndBasketInfo.getAttribute("data-user-mail");
+  var token = userAndBasketInfo.getAttribute("data-user-token");
+  var basketId = this.getAttribute("data-basket-id");
+  var elementToChange = document.getElementById("basket-status-".concat(basketId))
+  var status = "Accepted by FoodTruck";
 
-//   sendPostRequestStatus(basketId, email, token, status, elementToChange);
-// }
+  sendPostRequestStatus(basketId, email, token, status, elementToChange);
+}
 
-// function onClickDecline () {
-//   var userAndBasketInfo = document.getElementById("user-and-basket-info");
-//   var email = userAndBasketInfo.getAttribute("data-user-mail");
-//   var token = userAndBasketInfo.getAttribute("data-user-token");
-//   var basketId = this.getAttribute("data-basket-id");
-//   var elementToChange = document.getElementById("basket-status-".concat(basketId))
-//   var status = "Declined by FoodTruck";
+function onClickDecline () {
+  var userAndBasketInfo = document.getElementById("user-and-basket-info");
+  var email = userAndBasketInfo.getAttribute("data-user-mail");
+  var token = userAndBasketInfo.getAttribute("data-user-token");
+  var basketId = this.getAttribute("data-basket-id");
+  var elementToChange = document.getElementById("basket-status-".concat(basketId))
+  var status = "Declined by FoodTruck";
 
-//   sendPostRequestStatus(basketId, email, token, status, elementToChange);
-// }
+  sendPostRequestStatus(basketId, email, token, status, elementToChange);
+}
 
-// function eventManagerStatus(){
-//   var allAcceptButtons = document.querySelectorAll(".fa.fa-check");
-//   var allDeclineButtons = document.querySelectorAll(".fa.fa-times");
+function eventManagerStatus(){
+  var allAcceptButtons = document.querySelectorAll(".fa.fa-check");
+  var allDeclineButtons = document.querySelectorAll(".fa.fa-times");
 
-//   allAcceptButtons.forEach(function(btn) {
-//     btn.addEventListener("click", onClickAccept);
-//   });
-//   allDeclineButtons.forEach(function(btn) {
-//     btn.addEventListener("click", onClickDecline);
-//   });
+  allAcceptButtons.forEach(function(btn) {
+    btn.addEventListener("click", onClickAccept);
+  });
+  allDeclineButtons.forEach(function(btn) {
+    btn.addEventListener("click", onClickDecline);
+  });
 
-// }
+}
 
-// document.addEventListener("DOMContentLoaded", function() {
-//   eventManagerStatus();
-// });
+document.addEventListener("DOMContentLoaded", function() {
+  eventManagerStatus();
+});

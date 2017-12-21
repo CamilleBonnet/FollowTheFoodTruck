c45ab9508b220abd8c313a4cc3857476f108d3c8
Camille Bonnet <bonnet.cam@gmail.com>
2017-12-21 16:38:38 +0100

    distance fix

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -18,8 +18,7 @@ class TrucksController < ApplicationController
       @addresses = []
       all_addresses.each do |address|
         @trucks << address.truck unless address.active_address.nil?
-        @addresses << address unless address.active_address.nil?
-        raise
+        @addresses << address.active_address unless address.active_address.nil?
       end
     end
 
